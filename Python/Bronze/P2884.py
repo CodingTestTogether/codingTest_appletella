14460400e1201165a70417773c88b2fe4504d79d
AH_L <ihpb1727@gmail.com>
2024-03-18 22:43:53 +0900

    알람시계

--- Python/Bronze/P2884.py
+++ Python/Bronze/P2884.py
@@ -1,1 +1,16 @@
 #알람시계
+
+time = input().split(' ')
+H = int(time[0])
+M = int(time[1])
+
+if(M - 45 < 0):
+    if(H != 0):
+        print(H-1, M - 45 +60)
+    else:
+        print(23, M - 45 +60)
+else:
+    print(H, M - 45)
+    
+
+
